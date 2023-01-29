340c4d03084d1f372b319bdead019c9cc6874e89
Wosten01 <mribloko@gmail.com>
2023-01-29 08:40:33 +0400

    first commit

---
+++ src/main/java/com/example/four_in_a_row/FourInARowApplication.java
@@ -0,0 +1,13 @@
+package com.example.four_in_a_row;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FourInARowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FourInARowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/four_in_a_row/FourInARowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.four_in_a_row;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FourInARowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
